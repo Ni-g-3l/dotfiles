550e1bda63da97629729f0a4c9e79302c80967aa
Nig3l <nig3lpro@gmail.com>
2025-08-10 23:23:00 +0200

    Add basic alias

---
+++ .config/yadm/alias.sh
@@ -0,0 +1,25 @@
+# Alias declaration
+
+export EDITOR="nvim"
+
+function open_editor {
+	zellij action new-tab -l editor -c $(pwd)
+}
+
+function open_git_ui {
+	zellij action new-pane -f -- lazygit
+}
+
+## System 
+
+alias "s."="source ~/.bashrc"
+alias "u."='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y'
+
+## Tools
+
+alias "g."="git"
+alias "d."="htop"
+alias "f."="yazi $1"
+alias "lg."="open_git_ui"
+alias "e."="open_editor"
+
