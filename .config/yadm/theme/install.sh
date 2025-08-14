d26acccf0a5125a2752f84b6067ce7165b5e42df
Nig3l <nig3lpro@gmail.com>
2025-08-14 19:25:41 +0200

    Theme: Set Lock screen wallpaper

--- .config/yadm/theme/install.sh
+++ .config/yadm/theme/install.sh
@@ -6,23 +6,10 @@ plasma-apply-lookandfeeltool --apply Nothing
 plasma-apply-wallpaperimage $YADM_THEME_FOLDER/wallpaper.png
 plasma-apply-desktoptheme Nothing 
 plasma-apply-colorscheme Nothing
-kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key theme "Nothing"
+
+kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key theme "Nothing"
+kwriteconfig6 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.k de.image --group General --key Image $YADM_THEME_FOLDER/wallpaper.png
 
 qdbus org.kde.KWin /KWin reconfigure
 kwin_x11 --replace &
 
-# mkdir -p $HOME/.local/share/plasma/desktoptheme
-
-# curl -Lo $THEME_ARCHIVE https://gitlab.com/jomada/nothing/-/archive/main/nothing-main.zip
-
-# unzip $THEME_ARCHIVE
-
-# cp nothing-main/Nothing $HOME/.local/share/plasma/desktoptheme -r
-# cp nothing-main/color-schemes/* $HOME/.local/share/color-schemes
-# cp nothing-main/wallpapers/* $HOME/.local/share/wallpapers -r
-
-# mkdir $HOME/.local/share/aurorae/themes -p
-# cp nothing-main/aurorae/Nothing $HOME/.local/share/aurorae/themes -r
-
-
-# rm $THEME_ARCHIVE nothing-main -rf
