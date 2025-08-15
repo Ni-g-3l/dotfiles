68eb2de4206554de52bc7a564651291c06f18cbd
Nig3l <nig3lpro@gmail.com>
2025-08-15 12:03:13 +0200

    Remove Nothing theme

--- .config/yadm/theme/install.sh
+++ .config/yadm/theme/install.sh
@@ -2,14 +2,24 @@ YADM_FOLDER=$HOME/.config/yadm
 YADM_THEME_FOLDER=$YADM_FOLDER/theme
 THEME_ARCHIVE=nothing.zip
 
-plasma-apply-lookandfeeltool --apply Nothing
+cd /tmp
+
+git clone git@github.com:vinceliuice/Graphite-gtk-theme.git
+cd Graphite-gtk-theme
+./install.sh
+
+cd /tmp
+git clone git@github.com:vinceliuice/Graphite-kde-theme.git
+cd Graphite-kde-theme
+./install.sh
+
+plasma-apply-lookandfeeltool --apply Graphite-dark
 plasma-apply-wallpaperimage $YADM_THEME_FOLDER/wallpaper.png
-plasma-apply-desktoptheme Nothing 
-plasma-apply-colorscheme Nothing
+plasma-apply-desktoptheme Graphite-dark
+plasma-apply-colorscheme Graphitedark
 
-kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key theme "Nothing"
-kwriteconfig6 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.k de.image --group General --key Image $YADM_THEME_FOLDER/wallpaper.png
+dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme 'string:Graphite-orange-Dark'
+kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key theme "Graphite-dark-round"
 
-qdbus org.kde.KWin /KWin reconfigure
-kwin_x11 --replace &
-
+qdbus org.kde.KWin /KWin reconfigure 
+konsole -e kquitapp5 plasmashell && kstart5 plasmashell --windowclass plasmashell --window Desktop
