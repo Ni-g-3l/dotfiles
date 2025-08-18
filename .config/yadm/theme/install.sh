152853b04aaf98e054046b1e0f924f3c650a51e7
Nig3l <nig3lpro@gmail.com>
2025-08-18 18:51:37 +0200

    Set Graphite theme

--- .config/yadm/theme/install.sh
+++ .config/yadm/theme/install.sh
@@ -8,18 +8,5 @@ git clone git@github.com:vinceliuice/Graphite-gtk-theme.git
 cd Graphite-gtk-theme
 ./install.sh
 
-cd /tmp
-git clone git@github.com:vinceliuice/Graphite-kde-theme.git
-cd Graphite-kde-theme
-./install.sh
+dconf write /org/gnome/shell/extensions/user-theme/name "'Graphite-orange-Dark'"
 
-plasma-apply-lookandfeeltool --apply Graphite-dark
-plasma-apply-wallpaperimage $YADM_THEME_FOLDER/wallpaper.png
-plasma-apply-desktoptheme Graphite-dark
-plasma-apply-colorscheme Graphitedark
-
-dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme 'string:Graphite-orange-Dark'
-kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key theme "Graphite-dark-round"
-
-qdbus org.kde.KWin /KWin reconfigure 
-konsole -e kquitapp5 plasmashell && kstart5 plasmashell --windowclass plasmashell --window Desktop
