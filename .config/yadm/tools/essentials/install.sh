f1d5ec9140d9b3fbbc8349d493c6c7ae780a62be
Nig3l <nig3lpro@gmail.com>
2025-08-19 19:46:03 +0200

    Chore: Move fzf and ripgrep to dedicated install

--- .config/yadm/tools/essentials/install.sh
+++ .config/yadm/tools/essentials/install.sh
@@ -9,16 +9,3 @@ if [ $tool_installed != 0 ]; then
     curl https://sh.rustup.rs -sSf | sh
 fi
 
-fzf --version
-tool_installed=$?
-if [ $tool_installed != 0 ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
-fi
-
-rg --version
-tool_installed=$?
-if [ $tool_installed != 0 ]; then
-    sudo apt-get install ripgrep
-fi
-
