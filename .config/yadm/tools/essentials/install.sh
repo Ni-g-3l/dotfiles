28071047b8c11d7ea573ba91db1af4e28a4aaad1
Nig3l <nig3lpro@gmail.com>
2025-08-24 11:07:22 +0200

    Update essentials

--- .config/yadm/tools/essentials/install.sh
+++ .config/yadm/tools/essentials/install.sh
@@ -1,7 +1,4 @@
-sudo apt install build-essential
-
-sudo apt-get install curl
-sudo apt-get install xclip
+sudo apt install build-essential git git-lfs curl xclip net-tools
 
 cargo -V 
 tool_installed=$?
@@ -9,3 +6,12 @@ if [ $tool_installed != 0 ]; then
     curl https://sh.rustup.rs -sSf | sh
 fi
 
+v --version
+v_installed=$?
+if [ $v_installed != 0 ]; then 
+	git clone --depth=1 https://github.com/vlang/v $HOME/.v
+	cd $HOME/.v
+	make
+	sudo ./v symlink
+	cd -
+fi
