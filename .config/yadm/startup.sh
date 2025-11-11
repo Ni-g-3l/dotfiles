41045822cf8fea145f1873c54ac3f70e378c2933
Nig3l <nig3lpro@gmail.com>
2025-11-11 12:58:48 +0100

    Add default nix source

--- .config/yadm/startup.sh
+++ .config/yadm/startup.sh
@@ -2,6 +2,8 @@ YADM_FOLDER=$HOME/.config/yadm
 
 source $YADM_FOLDER/alias.sh
 
+if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi
+
 . "$HOME/.cargo/env"
 
 export PATH=$PATH:/opt/nvim-linux-x86_64/bin
