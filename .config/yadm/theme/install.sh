2177f3f34bba6ad83c0c1b7716bd4f0bf0c33c9e
Nig3l <cots.max@gmail.com>
2026-04-05 03:12:17 +0200

    Remove usessell old config of yadm

--- .config/yadm/theme/install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install gtk2-engines-murrine flatpak -y
-
-sudo flatpak override --filesystem=$HOME/.themes
-sudo flatpak override --filesystem=$HOME/.icons
-sudo flatpak override --filesystem=xdg-config/gtk-4.0
--- .config/yadm/tools/dev/install.sh
+++
@@ -1,5 +0,0 @@
-curl -f https://zed.dev/install.sh | sh
-
-sudo snap install cmake
-
-flatpak install flathub com.rioterm.Rio
